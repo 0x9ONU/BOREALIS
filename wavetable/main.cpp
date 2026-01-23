a7a3a50984387954cf9e625a9f5b1fd66a44bdf1
0X9ONU <e-berei@onu.edu>
2026-01-23 16:22:16 -0500

    It somewhat works

--- wavetable/main.cpp
+++ wavetable/main.cpp
@@ -10,18 +10,40 @@ using namespace daisy::seed;
 
 constexpr float PI = 3.14159265358979323846f;
 
+//Intalizations
 static DaisySeed hw;
 Wavetable wt;
 volatile int state = 0;
 float storagevar[10];
+MidiUartHandler midi;
+
+/** FIFO to hold messages as we're ready to print them */
+FIFO<MidiEvent, 128> event_log;
+
+void GetMidiTypeAsString(MidiEvent& msg, char* str)
+{
+    switch(msg.type)
+    {
+        case NoteOff: strcpy(str, "NoteOff"); break;
+        case NoteOn: strcpy(str, "NoteOn"); break;
+        case PolyphonicKeyPressure: strcpy(str, "PolyKeyPres."); break;
+        case ControlChange: strcpy(str, "CC"); break;
+        case ProgramChange: strcpy(str, "Prog. Change"); break;
+        case ChannelPressure: strcpy(str, "Chn. Pressure"); break;
+        case PitchBend: strcpy(str, "PitchBend"); break;
+        case SystemCommon: strcpy(str, "Sys. Common"); break;
+        case SystemRealTime: strcpy(str, "Sys. Realtime"); break;
+        case ChannelMode: strcpy(str, "Chn. Mode"); break;
+        default: strcpy(str, "Unknown"); break;
+    }
+}
 
 
 void AudioCallback(AudioHandle::InputBuffer  in,
                    AudioHandle::OutputBuffer out,
                    size_t                    size)
 {
-    wt.SetIndex(0, 2);
-    wt.SetFreq(0, 3*220);
+    //wt.SetFreq(0, 3*220);
 
     for(size_t i = 0; i < size; i++)
     {
@@ -62,7 +84,21 @@ int main(void)
     }
 
     wt.Init(sample_rate, waves, num_waves);
+    wt.SetIndex(0, 2);
+    wt.SetFreq(0, 660);
     hw.PrintLine("Wavetable initialized");
+
+    // Initalize Midi and Start receiving
+    hw.PrintLine("Start Midi Initalization...");
+    MidiUartHandler::Config midi_config;
+    midi.Init(midi_config);
+    hw.PrintLine("Midi Initalization! Start Receiving.");
+    midi.StartReceive();
+
+    //Temp
+    
+    uint32_t now      = System::GetNow();
+    uint32_t log_time = System::GetNow();
 
     //Configure and initialize button
     Switch button1;
@@ -77,5 +113,69 @@ int main(void)
     while(1) {
         // Read Inputs, doesnt function right now, was using to update a variable to change frequency and wave shape
         System::Delay(1);
+
+        now = System::GetNow();
+
+        /** Process MIDI in the background */
+        midi.Listen();
+        /** Loop through any MIDI Events */
+        while(midi.HasEvents())
+        {
+            hw.PrintLine("I got an event!");
+            MidiEvent msg = midi.PopEvent();
+
+            /** Handle messages as they come in 
+             *  See DaisyExamples for some examples of this
+             */
+            switch(msg.type)
+            {
+                case NoteOn:
+                    // Do something on Note On events
+                    {
+                        uint8_t bytes[3] = {0x90, 0x00, 0x00};
+                        bytes[1] = msg.data[0];
+                        bytes[2] = msg.data[1];
+                        midi.SendMessage(bytes, 3);
+                        //Set frequency of wavetable oscillator
+                        wt.SetFreq(0, 440.0f * powf(2.0f, (msg.data[0] - 57) / 12.0f));
+                    }
+                    break;
+                case NoteOff:
+                    {
+                      uint8_t bytes[3] = {0x90, 0x00, 0x00};
+                        bytes[1] = msg.data[0];
+                        bytes[2] = msg.data[1];
+                        midi.SendMessage(bytes, 3);
+                        
+                        wt.SetFreq(0, 0);
+                    }
+                default: break;
+            }
+
+            /** Regardless of message, let's add the message data to our queue to output */
+            event_log.PushBack(msg);
+        }
+
+        /** Now separately, every 5ms we'll print the top message in our queue if there is one */
+        if(now - log_time > 5)
+        {
+            log_time = now;
+            if(!event_log.IsEmpty())
+            {
+                auto msg = event_log.PopFront();
+                char outstr[128];
+                char type_str[16];
+                GetMidiTypeAsString(msg, type_str);
+                sprintf(outstr,
+                        "time:\t%ld\ttype: %s\tChannel:  %d\tData MSB: "
+                        "%d\tData LSB: %d\n",
+                        now,
+                        type_str,
+                        msg.channel,
+                        msg.data[0],
+                        msg.data[1]);
+                hw.PrintLine(outstr);
+            }
+        }
     }
 }
